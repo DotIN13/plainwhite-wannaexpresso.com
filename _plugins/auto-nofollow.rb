ffda5c36fed3994277bb3d964e9101e7b7e4442b
DotIN13 <dotin13@126.com>
2021-03-01 15:37:11 +0800

    Add nofollow to external links

---
+++ _plugins/auto-nofollow.rb
@@ -0,0 +1,15 @@
+require 'kramdown'
+
+module Kramdown
+  class Converter::Html
+    alias super_convert_a convert_a
+    def convert_a(el, indent)
+      if el.attr['href'] =~ /^[A-Za-z0-9]+:/
+        el.attr['target'] = '_blank'
+        el.attr['rel'] = 'nofollow noopener noreferrer'
+      end
+
+      super_convert_a el, indent
+    end
+  end
+end
