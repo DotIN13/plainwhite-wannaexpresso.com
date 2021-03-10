1afd61ba2217037a56469af1595a2b1486a021fc
DotIN13 <dotin13@126.com>
2021-03-10 11:09:48 +0800

    Fix excerpt warning

--- _plugins/excerpt.rb
+++ _plugins/excerpt.rb
@@ -5,7 +5,7 @@ require 'nokogiri'
 module Excerpt
   def excerpt(html)
     @doc = Nokogiri::HTML html
-    remove_tags = %w[figure.highlight div.highlight h1 h2 h3 h4 h5 h6 em]
+    remove_tags = %w[figure.highlight div.highlight h1 h2 h3 h4 h5 h6 em details summary]
     remove_tags.each do |tag|
       @doc.css(tag).each { |node| node.content = '' }
     end
