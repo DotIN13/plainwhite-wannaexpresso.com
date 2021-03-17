e2b197479f5d2b79f1321f53eaaabfa0233f2837
DotIN13 <dotin13@126.com>
2021-03-17 11:54:41 +0800

    Upload static build

---
+++ dist/api/like.js
@@ -0,0 +1,36 @@
+import * as dynamoose from "dynamoose"
+import articles from "_site/app/assets/articles.json"
+
+dynamoose.aws.sdk.config.update({
+    "accessKeyId": process.env.DYNAMOID_KEY_ID,
+    "secretAccessKey": process.env.DYNAMOID_KEY_SECRET,
+    "region": "us-west-1"
+});
+
+const schema = new dynamoose.Schema({
+    "article_id": {
+        "type": String,
+        "hashKey": true,
+        "validate": (val) => Object.values(articles).indexOf(val) != -1
+    },
+    "identity": {
+        "type": String,
+        "rangeKey": true
+    }
+}, {
+    "timestamps": true
+})
+const WannaLikes = dynamoose.model("wanna_likes", schema, { "create": true, "throughput": 5, "prefix": "dynamoose_" })
+
+module.exports = async (req, res) => {
+    try {
+        const like = await WannaLikes.create({
+            "article_id": req.query.article_id,
+            "identity": req.headers["x-forwarded-for"] + req.headers["user-agent"]
+        })
+        res.status(200).send(like)
+    }
+    catch (err) {
+        res.status(500).send(err)
+    }
+}
