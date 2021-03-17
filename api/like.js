d9405c2965e2625f6607c0bdf60c383bd88be3b7
DotIN13 <dotin13@126.com>
2021-03-17 11:55:15 +0800

    Update like api

--- api/like.js
+++ api/like.js
@@ -1,5 +1,5 @@
 import * as dynamoose from "dynamoose"
-import articles from "_site/app/assets/articles.json"
+import articles from "app/assets/articles.json"
 
 dynamoose.aws.sdk.config.update({
     "accessKeyId": process.env.DYNAMOID_KEY_ID,
