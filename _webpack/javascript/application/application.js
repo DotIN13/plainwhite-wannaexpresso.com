71241d014eb4601e5a9acfd6f0c57fd68e90a4b0
DotIN13 <dotin13@126.com>
2021-03-17 20:03:49 +0800

    Add like button

--- _webpack/javascript/application/application.js
+++ _webpack/javascript/application/application.js
@@ -5,6 +5,34 @@ import avatarAvif from '/assets/img/orange.webp?sizes[]=105,sizes[]=150,sizes[]=
 import avatarWebp from '/assets/img/orange.webp?sizes[]=105,sizes[]=150,sizes[]=240&format=webp'
 const avatarSizes = "(max-width: 600px) 105px, (max-width: 1024px) 150px, (max-width: 1600px) 240px, 240px"
 
+let removeActive = (e) => e.target.classList.remove("active")
+let addActive = (e) => e.target.classList.add("active")
+let animateHeart = (e) => {
+    e.stopPropagation()
+    // Animate heart after clicking
+    e.target.classList.add("heart--animating")
+    // Try to like article
+    like(e.target)
+}
+
+function bindHeartActions(heart) {
+    heart.addEventListener("click", animateHeart)
+    heart.addEventListener("animationend", (e) => e.target.classList.remove("heart--animating"))
+    heart.addEventListener("mouseover", addActive)
+    heart.addEventListener("mouseleave", removeActive)
+}
+
+function like(heart) {
+    fetch(`/api/like?article_id=${heart.dataset.like}`).then((res) => {
+        if (res.ok) {
+            heart.classList.add("active")
+            heart.removeEventListener("mouseover", addActive)
+            heart.removeEventListener("mouseleave", removeActive)
+            heart.removeEventListener("click", animateHeart)
+        }
+    })
+}
+
 window.addEventListener('DOMContentLoaded', () => {
     /** Initialize fancybox */
     $('[data-fancybox="gallery"]').fancybox({
@@ -20,6 +48,9 @@ window.addEventListener('DOMContentLoaded', () => {
         width: 150,
         height: 150
     })
+
+    /** Heart-shaped like button animations */
+    document.querySelectorAll('.heart').forEach((el) => bindHeartActions(el))
 })
 
 /* CSS */
--- webpack.common.js
+++ webpack.common.js
@@ -23,7 +23,7 @@ module.exports = {
         },
     },
     output: {
-        path: path.resolve(__dirname, 'dist/assets/public'),
+        path: path.resolve(__dirname, 'assets/public'),
         filename: '[name]-bundle.js',
     },
     resolve: {
