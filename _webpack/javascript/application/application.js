119895d6834a8de8eba39b14ad1db05117056994
DotIN13 <dotin13@126.com>
2021-03-24 21:27:27 +0800

    Replace fancybox with micromodal

--- _webpack/javascript/application/application.js
+++ _webpack/javascript/application/application.js
@@ -1,28 +1,24 @@
 /* Modules */
-import "@fancyapps/fancybox";
 import { responsiveGenerator } from '../responsive-img';
 import avatarAvif from '/assets/img/orange.webp?sizes[]=105,sizes[]=150,sizes[]=240&format=avif';
 import avatarWebp from '/assets/img/orange.webp?sizes[]=105,sizes[]=150,sizes[]=240&format=webp';
 const avatarSizes = "(max-width: 600px) 105px, (max-width: 1024px) 150px, (max-width: 1600px) 240px, 240px";
 import { bindHeartActions, fetchLikes } from '../likes';
+import MicroModal from 'micromodal';
 
 window.addEventListener('DOMContentLoaded', () => {
-  /** Initialize fancybox */
-  $('[data-fancybox="gallery"]').fancybox({
-    buttons: [
-      'download',
-      'thumbs',
-      'close'
-    ]
-  });
-
-  /** Render avatar image */
+  // Render avatar image
   responsiveGenerator(document.getElementById('avatar'), [avatarAvif, avatarWebp], avatarSizes, false, {
     width: 150,
     height: 150
   });
 
-  /** Heart-shaped like button animations */
+  // Initialize micromodal
+  MicroModal.init({
+    awaitCloseAnimation: true
+  });
+
+  // Heart-shaped like button animations
   bindHeartActions();
   fetchLikes();
 });
