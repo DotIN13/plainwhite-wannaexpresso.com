6aa2dea2de330cc84098ffda0410dd2925514eb0
DotIN13 <dotin13@126.com>
2021-03-27 23:18:10 +0800

    Fix micromodal error blocking likes

--- _webpack/javascript/application/application.js
+++ _webpack/javascript/application/application.js
@@ -1,11 +1,11 @@
 /* Modules */
+import { h, render } from 'preact';
 import avatarAvif from '/assets/img/orange.webp?sizes[]=105,sizes[]=150,sizes[]=240&format=avif';
 import avatarWebp from '/assets/img/orange.webp?sizes[]=105,sizes[]=150,sizes[]=240&format=webp';
 const avatarSizes = "(max-width: 600px) 105px, (max-width: 1024px) 150px, (max-width: 1600px) 240px, 240px";
 import { Picture } from '../responsive-img';
 import { bindHeartActions, fetchLikes } from '../likes';
 import MicroModal from 'micromodal';
-import { h, render } from 'preact';
 
 window.addEventListener('DOMContentLoaded', () => {
   // Render avatar image
@@ -16,14 +16,18 @@ window.addEventListener('DOMContentLoaded', () => {
     sizes={avatarSizes}
   />, document.getElementById("avatar"));
 
-  // Initialize micromodal
-  MicroModal.init({
-    awaitCloseAnimation: true
-  });
-
   // Heart-shaped like button animations
   bindHeartActions();
   fetchLikes();
+
+  // Initialize micromodal
+  try {
+    MicroModal.init({
+      awaitCloseAnimation: true
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 /* CSS */
