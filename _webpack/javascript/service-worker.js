3a2544b85bd938afb46fce4b38e6ba111adafcaf
DotIN13 <dotin13@126.com>
2021-03-31 00:21:15 +0800

    Use NetworkOnly as precache fallback

--- _webpack/javascript/service-worker.js
+++ _webpack/javascript/service-worker.js
@@ -1,17 +1,16 @@
 import { registerRoute } from 'workbox-routing';
 import * as navigationPreload from 'workbox-navigation-preload';
-import { StaleWhileRevalidate } from 'workbox-strategies';
+import { StaleWhileRevalidate, NetworkOnly } from 'workbox-strategies';
 // Used for filtering matches based on status code, header, or both
 import { CacheableResponsePlugin } from 'workbox-cacheable-response';
-// Used to limit entries in cache, remove entries after a certain period of time
-import { precacheAndRoute } from 'workbox-precaching';
-import { googleFontsCache, offlineFallback, pageCache, staticResourceCache, imageCache } from 'workbox-recipes';
+import { precache, matchPrecache } from 'workbox-precaching';
+import { googleFontsCache, offlineFallback, pageCache, imageCache } from 'workbox-recipes';
 import placeholderImage from "/assets/img/placeholder.jpeg?sizes[]=1020&format=webp&publicPath=assets/public/img";
 
 navigationPreload.enable();
 
 // Use with precache injection
-precacheAndRoute(self.__WB_MANIFEST);
+precache(self.__WB_MANIFEST);
 
 addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
@@ -38,9 +37,19 @@ pageCache({
  *********************************************************
  */
 
-staticResourceCache({
-  cacheName: 'assets'
-});
+registerRoute(
+  ({ request }) => request.destination === 'style' || request.destination === 'script' || request.destination === 'worker',
+  async ({request, event, url}) => {
+    let response;
+    try {
+      response = await matchPrecache(url.href);
+      if (!response) throw new Error;
+    } catch (e) {
+      response = await new NetworkOnly().handle({request, event});
+    }
+    return response;
+  }
+);
 
 // Cache json with a Stale While Revalidate strategy
 registerRoute(
