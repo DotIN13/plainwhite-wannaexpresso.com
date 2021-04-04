42974785c4950e8c9dbd303eb26e0dc75423c47e
DotIN13 <dotin13@126.com>
2021-04-04 19:32:31 +0800

    Manage avatar image with JPT

--- _webpack/javascript/application/index.js
+++ _webpack/javascript/application/index.js
@@ -1,12 +1,6 @@
 /* Modules */
-import { h, render } from 'preact';
-import avatarAvif from '/_webpack/images/orange.webp?sizes[]=105,sizes[]=150,sizes[]=240&format=avif';
-import avatarWebp from '/_webpack/images/orange.webp?sizes[]=105,sizes[]=150,sizes[]=240&format=webp';
-import { Picture } from '../responsive-img';
 import { bindHeartActions, fetchLikes } from '../likes';
 import initWorkbox from './workbox-utilities';
-
-const avatarSizes = "(max-width: 600px) 105px, (max-width: 1024px) 150px, (max-width: 1600px) 240px, 240px";
 
 // Darkmode
 import {updateDark, bindDarkToggle} from "./darkmode";
@@ -23,14 +17,6 @@ application.register("portal", PortalController);
 window.addEventListener('DOMContentLoaded', () => {
   // Workbox
   initWorkbox();
-
-  // Render avatar image
-  render(<Picture
-    avif={avatarAvif}
-    webp={avatarWebp}
-    lazy={false}
-    sizes={avatarSizes}
-  />, document.getElementById("avatar"));
 
   // Darkmode
   bindDarkToggle();
