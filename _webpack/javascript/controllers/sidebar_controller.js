c992566b14e4388ecfcc72b980465cabda69bbbe
DotIN13 <dotin13@126.com>
2021-04-14 15:29:31 +0800

    Add animation to sidebar expansion

--- _webpack/javascript/controllers/sidebar_controller.js
+++ _webpack/javascript/controllers/sidebar_controller.js
@@ -2,7 +2,32 @@ import { Controller } from "stimulus";
 
 export default class extends Controller {
 
+  static targets = [
+    "sidebar",
+    "container"
+  ]
+
+  static values = {
+    shift: Boolean
+  }
+
+  setHeight() {
+    const heights = [this.containerTarget.scrollHeight, this.sidebarTarget.scrollHeight];
+    this.containerTarget.style.height = `${heights[Number(this.shiftValue)]}px`;
+  }
+
+  set shift(val) {
+    this.setHeight();
+    this.shiftValue = val;
+    this.element.classList.toggle("shift", val);
+    this.setHeight();
+  }
+
+  get shift() {
+    return this.shiftValue || false;
+  }
+
   toggle() {
-    this.element.classList.toggle("shift");
+    this.shift = !this.shift;
   }
 }
