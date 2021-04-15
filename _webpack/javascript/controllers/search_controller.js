c31d334d4100281d63a52dabebc4e35453bb5fed
DotIN13 <dotin13@126.com>
2021-04-15 11:46:32 +0800

    Fix ripple re-animating on navigation

--- _webpack/javascript/controllers/search_controller.js
+++ _webpack/javascript/controllers/search_controller.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import SimpleJekyllSearch from 'simple-jekyll-search';
 import { Controller } from "stimulus";
 
--- _webpack/javascript/controllers/sidebar_controller.js
+++ _webpack/javascript/controllers/sidebar_controller.js
@@ -22,21 +22,30 @@ export default class extends Controller {
 
   toggle(e) {
     this.shift = !this.shift;
-    if (this.shift) this.ripple(e, "ripple");
-    if (!this.shift) this.ripple(e, "unripple");
+    if (this.shift) this.ripple(e, "in");
+    if (!this.shift) this.ripple(e, "out");
   }
 
   // Button effects
-  ripple(event, className) {
+  ripple(event, direction) {
     this.clearRipple();
     const diameter = this.avatarTarget.clientWidth;
     const circle = this.currentRipple = document.createElement("span");
     const radius = diameter / 2;
-    circle.className = className;
+    circle.className = direction === "in" ? "ripple ripple-in" : "ripple ripple-out";
+    circle.addEventListener("animationend", () => this.handleRipple(circle, direction));
     circle.style.width = circle.style.height = `${diameter}px`;
     circle.style.left = `${event.offsetX - radius}px`;
     circle.style.top = `${event.offsetY - radius}px`;
     this.avatarTarget.appendChild(circle);
+  }
+
+  handleRipple(circle, direction) {
+    if (direction === "in") {
+      circle.className = "ripple ripple-in-end";
+    } else {
+      circle.remove();
+    }
   }
 
   clearRipple() {
