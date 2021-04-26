0b87c6c8664cd6ea27448386808cfb8b4f8026b5
DotIN13 <dotin13@126.com>
2021-04-26 18:47:07 +0800

    Add logging to love GET api

--- api/love/get.js
+++ api/love/get.js
@@ -4,8 +4,10 @@ import { replaceStream } from "../../dist/app/javascript/love_incubator_message.
 connectTo.smart();
 
 export default async (_req, res) => {
+  console.log("Retrieving love messages...");
   try {
     const messages = await LoveIncubator.query("week").eq(firstDay()).limit(10).exec();
+    if (messages) console.log("Retrieved");
     res.setHeader("Content-Type", "text/vnd.turbo-stream.html");
     res.status(200).send(replaceStream(messages));
   } catch (err) {
