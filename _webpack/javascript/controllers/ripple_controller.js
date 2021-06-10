647d38880e466e522886659763821a8c678f5b1b
DotIN13 <dotin13@126.com>
2021-06-10 12:22:01 +0800

    Fix easing when ripples change directions

--- _webpack/javascript/controllers/ripple_controller.js
+++ _webpack/javascript/controllers/ripple_controller.js
@@ -14,11 +14,16 @@ export default class extends Controller {
     pos: Array,
     totalFrames: Number,
     currentFrame: Number,
+    currentStep: Number,
   }
 
   initiateAnimation() {
     if (this.animation) this.stopAnimation();
     this.direction = this.expandValue ? 1 : -1;
+    this.beginingFrame = this.currentFrameValue;
+    this.beginingStep = this.currentStepValue;
+    this.frameOffset = (this.expandValue ? this.totalFramesValue : 0) - this.currentFrameValue;
+    this.stepOffset = (this.expandValue ? 1 : 0) - this.beginingStep;
     this.element.style.setProperty('--ripple-x', `${this.posValue[0]}px`);
     this.element.style.setProperty('--ripple-y', `${this.posValue[1]}px`);
     this.animate();
@@ -29,13 +34,17 @@ export default class extends Controller {
     this.animation = null;
   }
 
-  // WIP: easing should be based on offset, not absolute frame number
   animate() {
     this.currentFrameValue += this.direction;
-    const step = this.currentFrameValue / this.totalFramesValue;
-    const easedStep = this.direction > 0 ? easeCubicOut(step) : easeCubicInOut(step);
-    // console.log(`Frame ${this.currentFrameValue} / ${this.totalFramesValue}, step ${step}, easedStep ${easedStep}`);
-    this.element.style.setProperty("--ripple-step", `${(easedStep * 100).toFixed(2)}%`);
+    const normalizedTime = (this.currentFrameValue - this.beginingFrame) / this.frameOffset;
+    const easedTime = this.direction > 0 ? easeCubicOut(normalizedTime) : easeCubicInOut(normalizedTime);
+    // The final step should be based on the begining step and walked steps during the eased time
+    const step = this.beginingStep + easedTime * this.stepOffset;
+    // console.log(`begining with ${this.beginingFrame}, total offset ${this.offset}, current ${this.currentFrameValue}`);
+    // console.log(`Frame ${this.currentFrameValue} / ${this.totalFramesValue}, time ${normalizedTime}, easedTime ${easedTime}, easedPercent ${position}`);
+    // console.log(`Begining Step ${this.beginingStep}, currentStep ${step}`);
+    this.element.style.setProperty("--ripple-step", `${(step * 100).toFixed(2)}%`);
+    this.currentStepValue = step;
     if (!(this.currentFrameValue % this.totalFramesValue)) {
       this.stopAnimation();
       return;
