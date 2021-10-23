3df1336ea07f5d67a50331fde080fdcce30abd45
DotIN13 <dotin13@126.com>
2021-10-23 21:27:03 +0800

    Allow moods with no header images

--- _webpack/javascript/controllers/mood_controller.js
+++ _webpack/javascript/controllers/mood_controller.js
@@ -29,70 +29,86 @@ export default class extends Controller {
   }
 
   initialize() {
-    this.moodImg = document.querySelector(`.mood[data-mood-id='${this.idValue}'] .mood__cover img`);
+    this.pinMoodImg();
+    if (!this.hasMoodImgTarget) {
+      this.animateCover();
+    }
+  }
+
+  pinMoodImg() {
+    this.moodImg = document.querySelector(`.mood[data-mood-id='${this.idValue}'] .mood__cover`);
     this.moodImgRect = this.moodImg.getBoundingClientRect().toJSON();
   }
 
   // Get boundingClientRect for portal image
   pinPortalImg() {
-    this.portalImg = this.element.querySelector(".mood__cover img");
+    this.portalImg = this.element.querySelector(".mood__cover");
     this.portalImgRect = this.portalImg.getBoundingClientRect().toJSON();
   }
 
-  imgLoaded() {
+  animateCover() {
     this.pinPortalImg();
-    this.posImgAtStart();
+    // this.posImgAtStart();
     this.posImgAtEnd();
   }
 
-  posImgAtStart() {
-    // Rect Example
-    // bottom: 346.890625
-    // height: 162.828125
-    // left: 363.4375
-    // right: 677.96875
-    // top: 184.0625
-    // width: 314.53125
-    // x: 363.4375
-    // y: 184.0625
-    // Append px
-    const moodImgRectStyle = this.rectToStyle(this.moodImgRect);
-    // Remove x and y properties
-    Object.assign(this.portalImg.style, moodImgRectStyle);
-    // Flush computed style
-    getComputedStyle(this.portalImg).width;
-  }
+  // posImgAtStart() {
+  //   // Rect Example
+  //   // bottom: 346.890625
+  //   // height: 162.828125
+  //   // left: 363.4375
+  //   // right: 677.96875
+  //   // top: 184.0625
+  //   // width: 314.53125
+  //   // x: 363.4375
+  //   // y: 184.0625
+  //   // Append px
+  //   const moodImgRectStyle = this.rectToStyle(this.moodImgRect);
+  //   // Remove x and y properties
+  //   Object.assign(this.portalImg.style, moodImgRectStyle);
+  //   // Flush computed style
+  //   getComputedStyle(this.portalImg).width;
+  // }
 
   posImgAtEnd() {
-    // Set imgAnimating to true when animation starts
+    // Calculate translate params
+    const translateX = this.moodImgRect.left + this.moodImgRect.width / 2 - this.portalImgRect.left - this.portalImgRect.width / 2;
+    const translateY = this.moodImgRect.top + this.moodImgRect.height / 2 - this.portalImgRect.top - this.portalImgRect.height / 2;
+    const scaleX = this.moodImgRect.width / this.portalImgRect.width;
+    const scaleY = this.moodImgRect.height / this.portalImgRect.height;
+    this.portalImg.style.transform = `translate(${translateX}px, ${translateY}px) scale(${scaleX, scaleY})`;
     this.imgAnimating = true;
-    const portalImgRectStyle = this.rectToStyle(this.portalImgRect);
-    Object.assign(this.portalImg.style, portalImgRectStyle);
+    requestAnimationFrame(() => {
+      this.portalImg.style = "transition: transform .4s cubic-bezier(0.4, 0, 0, 1); transform: none;";
+    });
+    // const portalImgRectStyle = this.rectToStyle(this.portalImgRect);
+    // Object.assign(this.portalImg.style, portalImgRectStyle);
   }
 
-  rectToStyle(rect) {
-    const posAttributes = ["top", "left", "right", "bottom", "width", "height"];
-    const style = Object.fromEntries(
-      Object.entries(rect)
-        .filter(([k]) => posAttributes.includes(k))
-        .map(([k, v]) => [k, `${v}px`])
-    );
-    return {
-      ...style,
-      position: "fixed",
-      margin: 0,
-      padding: 0
-    };
-  }
+  // rectToStyle(rect) {
+  //   const posAttributes = ["top", "left", "right", "bottom", "width", "height"];
+  //   const style = Object.fromEntries(
+  //     Object.entries(rect)
+  //       .filter(([k]) => posAttributes.includes(k))
+  //       .map(([k, v]) => [k, `${v}px`])
+  //   );
+  //   return {
+  //     ...style,
+  //     position: "fixed",
+  //   };
+  // }
 
   fixImg(e) {
-    if (this.imgAnimatingValue && e.propertyName == "width") {
-      Object.assign(this.portalImg.style, {
-        position: "relative",
-        inset: null,
-        height: null,
-        width: null,
-      });
+    console.log(e);
+    if (this.imgAnimatingValue && e.propertyName == "transform") {
+      // Object.assign(this.portalImg.style, {
+      //   position: "relative",
+      //   inset: null,
+      //   height: null,
+      //   width: null,
+      //   transform: null,
+      //   transition: "none",
+      // });
       this.imgAnimated = true;
     }
   }
