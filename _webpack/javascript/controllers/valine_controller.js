f704c0260687bdf0ae6b09291a287e58b91b9c47
DotIN13 <dotin13@126.com>
2024-01-17 00:39:40 +0800

    Format valine controller

--- _webpack/javascript/controllers/valine_controller.js
+++ _webpack/javascript/controllers/valine_controller.js
@@ -1,27 +1,27 @@
-import Valine from 'valine';
+import Valine from "valine";
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-
   connect() {
     this.valine ||= new Valine();
-  
+
     this.valine.init({
       el: this.element,
-      appId: 'PSy1tmiW2mx0DpKo7psk67EN-9Nh9j0Va',
-      appKey: 'MskLH2QYVC2Kqj8aG3XJ7x7o',
+      appId: "PSy1tmiW2mx0DpKo7psk67EN-9Nh9j0Va",
+      appKey: "MskLH2QYVC2Kqj8aG3XJ7x7o",
       serverURLs: "https://psy1tmiw.lc-cn-e1-shared.com",
-      placeholder: 'Your comment here...',
-      avatar: 'identicon',
+      placeholder: "Your comment here...",
+      avatar: "identicon",
       visitor: true,
-      lang: 'en',
-      path: window.location.pathname
+      lang: "en",
+      path: window.location.pathname,
     });
 
-    if (document.querySelector('.vicon')) {
-      document.querySelector(".vicon.vemoji-btn").innerHTML = '';
-      document.querySelector(".vicon.vpreview-btn").innerHTML = '';
-      let markdownLink = document.querySelector(".vpanel .markdown").parentElement;
+    if (document.querySelector(".vicon")) {
+      document.querySelector(".vicon.vemoji-btn").innerHTML = "";
+      document.querySelector(".vicon.vpreview-btn").innerHTML = "";
+      let markdownLink =
+        document.querySelector(".vpanel .markdown").parentElement;
       markdownLink.ariaLabel = "Markdown Guidelines";
       markdownLink.rel = "nofollow noopener noreferrer";
       document.querySelector(".vpower.txt-right")?.remove();
